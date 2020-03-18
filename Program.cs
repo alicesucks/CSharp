fa9f791754a46d7156ebaf72e197fb166b10b080
Alisa <57955448+alicesucks@users.noreply.github.com>
2020-03-18 16:52:37 +0200

    Update Program.cs
    added 5 new methods

--- Program.cs
+++ Program.cs
@@ -367,30 +367,275 @@ namespace Arrays_in_CSharp
             }
             Console.WriteLine();
         }
+        
+        // adding new
+        
+        public static void Num12()
+        {
+            /* 2 arrays of M and N size, create 3rd array with repeated elements only 1 time */
+            Random rand = new Random();
+            Console.WriteLine("Enter M size:");
+            int M = Int32.Parse(Console.ReadLine());
+            int[] mas1 = new int[M];
+            Console.WriteLine("Enter N size:");
+            int N = Int32.Parse(Console.ReadLine());
+            int[] mas2 = new int[N];
+            Console.WriteLine("Array 1:");
+            for (int i = 0; i < mas1.Length; i++)
+            {
+                mas1[i] = rand.Next(10);
+                Console.Write("{0,3}", mas1[i]);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Array 2:");
+            for (int i = 0; i < mas2.Length; i++)
+            {
+                mas2[i] = rand.Next(10);
+                Console.Write("{0,3}", mas2[i]);
+            }
+            Console.WriteLine();
+            int[] mastemp = new int[M + N];
+            int index = 0;
+            bool povtor = false;
+            for (int i = 0; i < mas1.Length; i++)
+            {
+                for (int j = 0; j < mas2.Length; j++)
+                {
+                    if (mas1[i] == mas2[j])
+                    {
+                        /* loop for guessing arr3 size */
+                        for (int k = 0; k < mastemp.Length; k++)
+                        {
+                            if (mastemp[k] == mas2[j])
+                            {
+                                povtor = true;
+                            }
+                        }
+                        /* if(!povtor) means index++ to create arr3 with size of 
+                         * repeated elements of 1st and 2nd only 1 time */
+                        if (!povtor)
+                        {
+                            mastemp[index++] = mas2[j];
+                        }
+                        povtor = false;
+                    }
+                }
+            }
+            int[] mas3 = new int[index];
+            for (int i = 0; i < mas3.Length; i++)
+            {
+                mas3[i] = mastemp[i];
+            }
+            Console.WriteLine("Array 3:");
+            foreach (var a in mas3)
+            {
+                Console.Write("{0,3}", a);
+            }
+            Console.WriteLine();
+        }
+        public static void Num13()
+        {
+            Random rand = new Random();
+            Console.WriteLine("Enter M size:");
+            int M = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter N size:");
+            int N = Int32.Parse(Console.ReadLine());
+            int[,] arr = new int[M, N];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rand.Next(100);
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Enter 1 for right shift, enter 2 for left shift:");
+            int shift = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Choose how many shifts do you want to perform:");
+            int shiftsAmount = Int32.Parse(Console.ReadLine());
+            int temp = new int();
+            if (shift == 1)
+            { // right shift
+                /*1st loop for shifts*/
+                for (int x = 0; x < shiftsAmount; x++)
+                {
+                    /*loop for shifting array elements*/
+                    for (int i = 0; i < arr.GetLength(0); i++)
+                    {
+                        for (int j = arr.GetLength(1) - 1; j > 0; j--)
+                        { // starting from back of an array
+                            temp = arr[i, j - 1]; // remember the last element
+                            arr[i, j - 1] = arr[i, j]; // shifting
+                            arr[i, j] = temp; // first elements will be the "remembered" last
+                        }
+                    }
+                }
+            }
+            else if (shift == 2)
+            { // left shift
+                for (int x = 0; x < shiftsAmount; x++)
+                {
+                    for (int i = 0; i < arr.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+                        { // starting from the beginning
+                            temp = arr[i, j + 1];
+                            arr[i, j + 1] = arr[i, j];
+                            arr[i, j] = temp;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Incorrect input!");
+                return;
+            }
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static void Num14()
+        {
+            Random rand = new Random();
+            int[,] arr = new int[5, 5];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rand.Next(-100, 100);
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int maxIndexI = 0;
+            int maxIndexJ = 0;
+            int minIndexI = 0;
+            int minIndexJ = 0;
+            /* detecting max and min elements of an array */
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > arr[maxIndexI, maxIndexJ])
+                    {
+                        // if current element is bigger than 0,0 -> we rewrite maxIndexI and maxIndexJ
+                        maxIndexI = i;
+                        maxIndexJ = j;
+                    }
+                    if (arr[i, j] < arr[minIndexI, minIndexJ])
+                    {
+                        // if current element if smaller -> we rewrite min indexes
+                        minIndexI = i;
+                        minIndexJ = j;
+                    }
+                }
+            }
+            Console.WriteLine("Max element of an array is " + arr[maxIndexI, maxIndexJ]
+                + " index is: " + maxIndexI + "\\" + maxIndexJ);
+            Console.WriteLine("Min element of an array is " + arr[minIndexI, minIndexJ]
+                + " index is: " + minIndexI + "\\" + minIndexJ);
+            int sum = new int();
+            bool p = false;
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (i == maxIndexI && j == maxIndexJ || i == minIndexI && j == minIndexJ)
+                    {
+                        p = !p;
+                        continue;
+                    }
+                    else if (p)
+                    {
+                        sum += arr[i, j];
+                    }
+                }
+            }
+            Console.WriteLine("Sum of elements between max and min = " + sum);
+        }
+        public static void Num15()
+        {
+            Random rand = new Random();
+            int[,] arr = new int[5, 5];
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    arr[i, j] = rand.Next(0, 100);
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            int[] mas = new int[5];
+            int sum = 0;
+            for (int j = 0, x = 0; j < arr.GetLength(1); j++) {
+                for (int i = 0; i < arr.GetLength(0); i++) {
+                    sum += arr[i, j];
+                }
+                mas[x++] = sum;
+                sum = 0;
+            }
+            Console.WriteLine("Array with sorted sums by columns:");
+            foreach(var s in mas) {
+                Console.Write(s + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < mas.Length; i++) {
+                for (int j = mas.Length - 1; j > i; j--) {
+                    if (mas[j - 1] < mas[j]) {
+                        int temp = mas[j - 1];
+                        mas[j - 1] = mas[j];
+                        mas[j] = temp;
+                        for (int x = 0; x < arr.GetLength(0); x++) {
+                            for (int z = 0; z < arr.GetLength(1); z++) {
+                                int temp2 = arr[x, j - 1];
+                                arr[x, j - 1] = arr[x, j];
+                                arr[x, j] = temp2;
+                            }
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Sorted array:");
+            for(int i = 0; i < arr.GetLength(0); i++) {
+                for(int j = 0; j < arr.GetLength(1); j++) {
+                    Console.Write(arr[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Num16()
+        {
+            int[][] arr = new int[9][];
+            arr[0] = new int[] { 1 };
+            arr[1] = new int[] { 2, 2 };
+            arr[2] = new int[] { 3, 3, 3 };
+            arr[3] = new int[] { 4, 4, 4, 4 };
+            arr[4] = new int[] { 5, 5, 5, 5, 5 };
+            arr[5] = new int[] { 6, 6, 6, 6, 6, 6 };
+            arr[6] = new int[] { 7, 7, 7, 7, 7, 7, 7 };
+            arr[7] = new int[] { 8, 8, 8, 8, 8, 8, 8, 8 };
+            arr[8] = new int[] { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
+            for(int i = 0; i < arr.Length; i++)
+            {
+                for(int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.Write(arr[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
         public static void Main(string[] args)
         {
-            Num1();
-            Console.WriteLine();
-            Num2();
-            Console.WriteLine();
-            Num3();
-            Console.WriteLine();
-            Num4();
-            Console.WriteLine();
-            Num5();
-            Console.WriteLine();
-            Num6();
-            Console.WriteLine();
-            Num7();
-            Console.WriteLine();
-            Num8();
-            Console.WriteLine();
-            Num9();
-            Console.WriteLine();
-            Num10();
-            Console.WriteLine();
-            Num11();
-            Console.WriteLine();
+            
         }
     }
 }
